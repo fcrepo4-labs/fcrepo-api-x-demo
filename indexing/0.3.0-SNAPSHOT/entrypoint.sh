c40d639fe7c96e6c9319e2c70ac9c252c4a35f7f
Aaron Birkland <apb@jhu.edu>
2017-07-07 14:14:10 -0400

    Fix entrypoint to remove old karaf pids

--- indexing/0.3.0-SNAPSHOT/entrypoint.sh
+++ indexing/0.3.0-SNAPSHOT/entrypoint.sh
@@ -6,6 +6,8 @@
 sed -e "s:^org.ops4j.pax.url.mvn.localRepository=.*:org.ops4j.pax.url.mvn.localRepository=${MAVEN_REPO}:" \
         -i etc/org.ops4j.pax.url.mvn.cfg
 
+rm -rf instances/*
+
 echo "#empty" > /etc/hosts
 
 replace_envs() {
