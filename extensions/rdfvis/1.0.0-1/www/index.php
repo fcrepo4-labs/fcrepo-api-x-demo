09afcaa29d45f007286d5c1b0fb1e19cb1cb5cfa
Aaron Birkland <apb@jhu.edu>
2017-09-18 18:11:29 -0400

    Properly handle authn in rdfvis php extension
    Easyrdf was making unauthenticated requests to Fedora.  This fixes the
    situation so that the _provided_ credentials (all headers, really) are
    used to formulate the request for a given resource.

--- extensions/rdfvis/1.0.0-1/www/index.php
+++ extensions/rdfvis/1.0.0-1/www/index.php
@@ -9,27 +9,62 @@
     set_include_path(get_include_path() . PATH_SEPARATOR . './lib/');
     require_once "EasyRdf.php";
 
+
+    function getGraph($resource) {
+
+        $headers = array();
+        foreach($_SERVER as $key => $value) {
+            if (strpos($key, 'HTTP_') === 0 && $key !== "HTTP_ACCEPT") {
+                $header = str_replace(' ', '-', ucwords(str_replace('_', ' ', strtolower(substr($key, 5)))));
+                $headers[] = $header . ": " . $value;
+            }
+        }
+        
+        $headers['Accept'] = "Accept: application/n-triples";
+
+        $curl = curl_init($resource);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+    
+        $rdf = curl_exec($curl);
+        $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+        
+        
+        if ($httpCode <= 200 || $httpCode > 299) {
+            error_log("Got bad error code" . $httpCode);
+            http_response_code($httpCode);
+            exit("Could not retrieve <" . $resource . ">; code " . $httpCode . "\n Body: " . $rdf );
+        }
+
+        $graph = new EasyRdf_Graph();
+        $graph->parse($rdf, "nt");
+
+        return $graph;
+    }
+
     $path = preg_replace('/^\/rdfVis/', '', $_SERVER['REQUEST_URI']);
     $resource = $_SERVER['HTTP_APIX_RESOURCE'];
 
     if ($path == "/image") {
-        $graph = new EasyRdf_Graph($resource);
-        $graph->load();
+        
+        $graph = getGraph($resource);
+
         $gv = new EasyRdf_Serialiser_GraphViz();
         $format = EasyRdf_Format::getFormat("svg");
         header("Content-Type: ".$format->getDefaultMimeType());
         echo $gv->renderImage($graph, $format);
         return;
     } else if ($path == "/text") {
-        $graph = new EasyRdf_Graph($resource);
-        $graph->load();
+        $graph = getGraph($resource);
+
         $ttl = new EasyRdf_Serialiser_Turtle();
         echo $ttl->serialise($graph, "turtle");
         header("Content-Type: text/plain");
         return;
     }
 
-    header("Content-Type: text/html")
+    header("Content-Type: text/html");
 ?>
 <!DOCTYPE html>
 <html>
